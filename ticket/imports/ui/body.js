fb3b4c3a49bd8a8acbfedb5a2573e558acaae67b
rosenj5 <rosenj5@rpi.edu>
2016-11-14 18:56:11 -0500

    Update tabbing

--- ticket/imports/ui/body.js
+++ ticket/imports/ui/body.js
@@ -5,91 +5,91 @@ import { Tickets } from '../api/tickets.js';
 import './body.html';
 
 Router.route('/', function(){
-    this.render('ticketview');
+	this.render('ticketview');
 });
 
 Router.route('/add', function(){
-    this.render('submit');
+	this.render('submit');
 });
 
 Template.ticketview.helpers({
-    tickets() {
-        return Tickets.find({}, { sort: { createdAt: -1 } });;
-    },
+	tickets() {
+		return Tickets.find({}, { sort: { createdAt: -1 } });;
+	},
 });
 
 Template.ticketview.events({
-    'click .ticket-list .tbtn': function(event){
-	const target = event.target;
-	$(target).parent().parent().parent().find("ul").toggle();
-    }
+	'click .ticket-list .tbtn': function(event){
+		const target = event.target;
+		$(target).parent().parent().parent().find("ul").toggle();
+	}
 });
 
 Template.submit.onCreated(function submitOnCreated(){
-    //variable to keep track of final priority value
-    this.prioritysel = new ReactiveVar("D");
-    this.priorityclass = new ReactiveVar("btn-info");
+	//variable to keep track of final priority value
+	this.prioritysel = new ReactiveVar("D");
+	this.priorityclass = new ReactiveVar("btn-info");
 });
 
 Template.submit.helpers({
-    prioritysel() {
-	return Template.instance().prioritysel.get();
-    },
-    priorityclass() {
-	return Template.instance().priorityclass.get();
-    }
+	prioritysel() {
+		return Template.instance().prioritysel.get();
+	},
+	priorityclass() {
+		return Template.instance().priorityclass.get();
+	}
 });
 
 Template.submit.events({
-    'click .pri-btn': function(event) {
+	'click .pri-btn': function(event) {
+		event.preventDefault();
+		const target = event.target;
+		const val = target.innerHTML;
+		if (val == "Priority A"){
+			Template.instance().prioritysel.set("A");
+			Template.instance().priorityclass.set("btn-danger");
+		}
+		else if (val == "Priority B"){
+			Template.instance().prioritysel.set("B");
+			Template.instance().priorityclass.set("btn-warning");
+		}
+		else if (val == "Priority C"){
+			Template.instance().prioritysel.set("C");
+			Template.instance().priorityclass.set("btn-primary");
+		}
+		else if (val == "Priority D"){
+			Template.instance().prioritysel.set("D");
+			Template.instance().priorityclass.set("btn-info");
+		}
+	},
+	'click #finalpriority': function(event){
 	event.preventDefault();
-	const target = event.target;
-	const val = target.innerHTML;
-	if (val == "Priority A"){
-	    Template.instance().prioritysel.set("A");
-            Template.instance().priorityclass.set("btn-danger");
-	}
-	else if (val == "Priority B"){
-            Template.instance().prioritysel.set("B");
-            Template.instance().priorityclass.set("btn-warning");
-        }
-	else if (val == "Priority C"){
-            Template.instance().prioritysel.set("C");
-            Template.instance().priorityclass.set("btn-primary");
-        }
-	else if (val == "Priority D"){
-            Template.instance().prioritysel.set("D");
-            Template.instance().priorityclass.set("btn-info");
-        }
-    },
-    'click #finalpriority': function(event){
-	event.preventDefault();
-    },
-    'click #submit-btn'(event) {
-        event.preventDefault();
-        const target = event.target.parentElement;
-        const description = target.description.value;
-	const priority = target.finalpriority.innerHTML;
-	const youremail = target.youremailin.value;
-	const rpiemail = target.rpiemailin.value;
-	const issuetype = target.issuetype.value;
-	const status = "new-ticket";
-	if (description == "" || priority == "" || youremail == "" || rpiemail == "" || issuetype == ""){
-	    alert("Something wasn't set, try again!");
-	    return false;
-	}
-	Tickets.insert({
-            description,
-            youremail,
-	    rpiemail,
-	    issuetype,
-	    priority,
-	    status,
-            createdAt: new Date(),
-        });
+	},
+	'click #submit-btn'(event) {
+		event.preventDefault();
+		const target = event.target.parentElement;
+		const description = target.description.value;
+		const priority = target.finalpriority.innerHTML;
+		const youremail = target.youremailin.value;
+		const rpiemail = target.rpiemailin.value;
+		const issuetype = target.issuetype.value;
+		const status = "new-ticket";
+		if (description == "" || priority == "" || youremail == "" || rpiemail == "" || issuetype == ""){
+			alert("Something wasn't set, try again!");
+			return false;
+		}
+		Tickets.insert({
+			description,
+			youremail,
+			rpiemail,
+			issuetype,
+			priority,
+			status,
+			createdAt: new Date(),
+		});
 
-        //target.titleinput.value = "";
-        //target.bodyinput.value = "";
-        Router.go('/');
-    },
+		//target.titleinput.value = "";
+		//target.bodyinput.value = "";
+		Router.go('/');
+	},
 });
