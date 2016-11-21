54008ba2b6aa6ae73c4573cf49f9b32a044138d7
Maxwell Schmitt <maxwells@mellifluous.org>
2016-11-20 19:22:37 -0500

    Fixed emacs indentation.

--- ticket/imports/ui/body.js
+++ ticket/imports/ui/body.js
@@ -5,26 +5,26 @@ import { Tickets } from '../api/tickets.js';
 import './body.html';
 
 function badform() {
-      $(document).ready(function(){
+    $(document).ready(function(){
         toastr;
         toastr.options = {
-                        "closeButton": false,
-                        "debug": false,
-                        "newestOnTop": true,
-                        "progressBar": true,
-                        "positionClass": "toast-top-full-width",
-                        "preventDuplicates": true,
-                        "showDuration": "3000",
-                        "hideDuration": "3000",
-                        "timeOut": "3000",
-                        "extendedTimeOut": "3000",
-                        "showEasing": "swing",
-                        "hideEasing": "linear",
-                        "showMethod": "fadeIn",
-                        "hideMethod": "fadeOut"
+            "closeButton": false,
+            "debug": false,
+            "newestOnTop": true,
+            "progressBar": true,
+            "positionClass": "toast-top-full-width",
+            "preventDuplicates": true,
+            "showDuration": "3000",
+            "hideDuration": "3000",
+            "timeOut": "3000",
+            "extendedTimeOut": "3000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
         }
-          toastr['warning']("Please fill out all fields!", "Empty Fields!");
-      });
+        toastr['warning']("Please fill out all fields!", "Empty Fields!");
+    });
 }
 
 Router.route('/', function () {
