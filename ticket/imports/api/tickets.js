75a87a431bcc5802538ee53f0136de19338cad6c
Maxwell Schmitt <maxwells@mellifluous.org>
2016-12-02 19:55:13 -0500

    Commented tickets.js

--- ticket/imports/api/tickets.js
+++ ticket/imports/api/tickets.js
@@ -6,7 +6,7 @@ import { Mongo } from 'meteor/mongo';
 export const Tickets = new Mongo.Collection('tickets');
 
 Meteor.methods({
-  'tickets.insert'(
+  'tickets.insert'( //secure method for insertion - take all fields from client
     namein,
     rpiemail,
     altemail,
@@ -23,7 +23,7 @@ Meteor.methods({
     if (!this.userId) {
       throw new Meteor.Error('not-authorized');
     }
-    Tickets.insert({
+    Tickets.insert({ //actually insert
       namein,
       rpiemail,
       altemail,
@@ -41,34 +41,34 @@ Meteor.methods({
   },
   'tickets.comment'(numtofind, body, author) {
     const ticket = Tickets.findOne({ number: numtofind }); // get the actual ticket
-    const arro = ticket.comments;
-    const time = new Date();
+    const arro = ticket.comments; //get the existing comments
+    const time = new Date(); //current time
     let arrnew = [{}];
-    if (typeof (arro) === 'undefined') {
-      arrnew = [{ author, body, time }];
-    } else {
-      arrnew = arro;
-      arrnew.push({ author, body, time });
+    if (typeof (arro) === 'undefined') { //if no existing comments
+      arrnew = [{ author, body, time }]; //compose new comment
+    } else { //there are existing comments
+      arrnew = arro; //copy old array
+      arrnew.push({ author, body, time }); //push new comment to array
     }
-    Tickets.update({ _id: ticket._id }, { $set: { comments: arrnew } });
+    Tickets.update({ _id: ticket._id }, { $set: { comments: arrnew } }); //update comments array to new comments array
   },
-  'tickets.resolve'(numtofind, body, open) {
-    const ticket = Tickets.findOne({ number: numtofind });
-    const arro = ticket.comments;
-    const author = 'System';
-    const time = new Date();
+  'tickets.resolve'(numtofind, body, open) { //function to resolve or reopen a ticket
+    const ticket = Tickets.findOne({ number: numtofind }); //find ticket
+    const arro = ticket.comments; //get existing comments
+    const author = 'System'; //these comments are authored by System
+    const time = new Date(); //current time
     let arrnew = [{}];
-    if (typeof (arro) === 'undefined') {
-      arrnew = [{ author, body, time }];
-    } else {
-      arrnew = arro;
-      arrnew.push({ author, body, time });
+    if (typeof (arro) === 'undefined') { //if no existing comments
+      arrnew = [{ author, body, time }]; //compose new comment
+    } else { //else comments already exist
+      arrnew = arro; //copy comments
+      arrnew.push({ author, body, time }); //add new comment to array
     }
-    Tickets.update({ _id: ticket._id }, { $set: { comments: arrnew } });
-    if (open) {
-      Tickets.update({ _id: ticket._id }, { $set: { status: true } });
-    } else {
-      Tickets.update({ _id: ticket._id }, { $set: { status: false } });
+    Tickets.update({ _id: ticket._id }, { $set: { comments: arrnew } }); //update comments array with new one
+    if (open) { //if reopening
+      Tickets.update({ _id: ticket._id }, { $set: { status: true } }); //set status to open
+    } else { //if resolving
+      Tickets.update({ _id: ticket._id }, { $set: { status: false } }); //set status to resolved
     }
   },
 });
